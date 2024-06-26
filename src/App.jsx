3c6cf42e7254bf4cb999746a959564f453a745c0
= <=>
2024-06-26 14:18:40 +0530

    add the structure of the website using html

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container-fluid">
+        <div className="row mt-5">
+          <div className="col-md-5 bg-secondary p-3 mx-auto rounded">
+            <div className="row">
+              <div className="col-md-12">
+                <h2 className="text-danger text-center mb-0">Password Generater</h2>
+              </div>
+              <div className="col-md-12 my-3">
+                <div className="input-group">
+                  <input type="text" className="form-control" />
+                  <button className="btn btn-primary">Copy</button>
+                </div>
+              </div>
+              <div className="col-4 d-flex align-items-center gap-2 justify-content-center">
+                <input type="range" name="length" id="length" className="form-range" />
+                <label htmlFor="length" className="form-label">Length</label>
+              </div>
+              <div className="col-4 d-flex align-items-center gap-2 justify-content-center">
+                <input type="checkbox" name="numbers" id="numbers" className="form-check" />
+                <label htmlFor="numbers" className="form-label">Numbers</label>
+              </div>
+              <div className="col-4 d-flex align-items-center gap-2 justify-content-center">
+                <input type="checkbox" name="character" id="character" className="form-check" />
+                <label htmlFor="character" className="form-label">character</label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
